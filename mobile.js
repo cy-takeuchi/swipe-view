75a920344118cf92f0e9a2901ecd1d7e89605f9f
Yoshihiko Takeuchi <yoshihiko-takeuchi@cybozu.co.jp>
2019-05-19 08:58:58 +0900

    Initial Commit

---
+++ mobile.js
@@ -0,0 +1,265 @@
+// プラグインの設定画面で入力時に表示するメッセージを変えれるように
+// 各ステップでデータ保存、ネットが切れても途中から入力できる
+jQuery.noConflict();
+(($) => {
+    'use strict';
+
+    let conn = new kintoneJSSDK.Connection();
+    let kintoneApp = new kintoneJSSDK.App(conn);
+    const appId = kintone.mobile.app.getId();
+
+    const swipeSpaceId = 'cy-swipe';
+    //const sliderSpaceId = 'cy-pager';
+    const listId = 'cy-ul';
+
+    let showSwipeArea = (el) => {
+        let style = '';
+        style += 'width: 100%; padding: 5px; line-height: 3; text-align: center;';
+        style += 'z-index: 999; position: absolute; bottom: 5px;';
+        style += 'background-color: gold; opacity: 0.6;';
+        let html = `<div id="${swipeSpaceId}" style="${style}">ここをスワイプぅ</div>`;
+        $(el).append(html);
+    }
+
+    class Form {
+        constructor() {
+            this.length = 0;
+            this.groupList = [];
+        };
+
+        async getLayout() {
+            let res = await kintoneApp.getFormLayout(appId);
+            return res.layout;
+        }
+
+        async grouping(layout) {
+            let array = [];
+            let all = [];
+            for (let i of Object.keys(layout)) {
+                let code = layout[i].fields[0].code;
+                let type = layout[i].fields[0].type;
+                let id = layout[i].fields[0].elementId;
+                let obj = {
+                    code: code,
+                    type: type
+                }
+
+                if (type === 'SPACER') {
+                    if (id === 'swipe' || id === 'pager') {
+                        continue;
+                    }
+                } else if (type === 'HR') {
+                    continue;
+                }
+
+                if (type === 'SPACER') {
+                    if (array.length > 0) {
+                        this.groupList.push(array);
+                        array = [];
+                    }
+                } else {
+                    array.push(obj);
+                    all.push(obj);
+                }
+            }
+
+            if (array.length > 0) {
+                this.groupList.push(array);
+                array = [];
+            }
+
+            // 未入力を設定
+            this.groupList.push(all);
+
+            // 全件を設定
+            this.groupList.push(all);
+        }
+
+        async parseForm() {
+            let layout = await this.getLayout();
+            console.log('layout', layout);
+
+            await this.grouping(layout);
+            console.log('groupList', this.groupList);
+        }
+
+        active(num) {
+            for (let i = 0; i < this.groupList[num].length; i++) {
+                let field = this.groupList[num][i];
+                kintone.mobile.app.record.setFieldShown(field.code, true);
+            }
+        }
+
+        passive(num) {
+            for (let i = 0; i < this.groupList[num].length; i++) {
+                let field = this.groupList[num][i];
+                kintone.mobile.app.record.setFieldShown(field.code, false);
+            }
+        }
+
+        passiveFirst() {
+            // 最初のグループリストは表示するから開始は1
+            for (let i = 1; i < this.groupList.length; i++) {
+                let group = this.groupList[i];
+                for (let j = 0; j < group.length; j++) {
+                    let field = group[j];
+                    kintone.mobile.app.record.setFieldShown(field.code, false);
+                }
+            }
+        }
+    }
+
+    class Pager {
+        constructor() {
+            this.current = 0;
+            this.min = 0;
+            this.max = 0;
+
+            this.setPage(0);
+        }
+
+        getMax() {
+            return this.max;
+        }
+
+        setMax(max) {
+            // 「+2」は未入力、全件分
+            this.max = max + 2;
+        }
+
+        getPage() {
+            return this.current;
+        }
+
+        setPage(num) {
+            this.current = num;
+        }
+    }
+
+    class List {
+        constructor(id) {
+            this.id = id;
+            this.el = `ul#${id} li`;
+            this.max = 0;
+        }
+
+        init() {
+            $(this.el).eq(0).click();
+        }
+
+        setMax(max) {
+            this.max = max;
+        }
+
+        show(el) {
+            let html = '';
+            html += '<div>';
+            html += `<ul id="${this.id}" style="display: inline-block; margin: 20px; padding: 0px;">`;
+            for (let i = 0; i < this.max - 2; i++) {
+                html += `<li style="display: inline; padding: 8px 16px;"><a href="javascript:void(0)">${i + 1}</a></li>`;
+            }
+            html += `<li style="display: inline; padding: 8px 16px;"><a href="javascript:void(0)">未入力項目</a></li>`;
+            html += `<li style="display: inline; padding: 8px 16px;"><a href="javascript:void(0)">全項目</a></li>`;
+            html += '</ul>';
+            html += '</div>';
+
+            $(el).append(html);
+        }
+
+        active(num) {
+            $(this.el).eq(num).css('background-color', 'gold');
+            $(this.el).eq(num).children('a').css('color', 'royalblue').css('font-weight', 'bold');
+        }
+
+        passive(num) {
+            $(this.el).eq(num).css('background-color', 'white');
+            $(this.el).eq(num).children('a').css('color', 'royalblue').css('font-weight', 'normal');
+        }
+    }
+
+
+
+    let form = new Form();
+
+    kintone.events.on(['mobile.app.record.create.show', 'mobile.app.record.edit.show'], (event) => {
+        console.log('abc');
+
+        // プラグインの設定値から取得する
+        let el = kintone.mobile.app.record.getSpaceElement('pager');
+        showSwipeArea(el);
+
+        let pager = new Pager();
+        let list = new List(listId);
+
+        form.parseForm().then(() => {
+            pager.setMax(form.groupList.length);
+            list.setMax(form.groupList.length);
+
+            list.show(el);
+            form.passiveFirst();
+            list.init();
+        });
+
+        $(document).on('click', `ul#${listId} li`, (event) => {
+            let before = pager.getPage();
+            let current = $(event.currentTarget).index();
+
+            list.passive(before);
+            form.passive(before);
+
+            list.active(current);
+            form.active(current);
+
+            pager.setPage(current);
+        });
+
+
+        $(`div#${swipeSpaceId}`).hammer().bind('swiperight', () => {
+            console.log('swipe right');
+            let before = pager.getPage();
+            let current = before + 1;
+            if (current >= pager.getMax() - 2) {
+                return;
+            }
+
+            list.passive(before);
+            form.passive(before);
+
+            list.active(current);
+            form.active(current);
+
+            pager.setPage(current);
+        });
+        $(`div#${swipeSpaceId}`).hammer().bind('swipeleft', () => {
+            console.log('swipe left');
+            let before = pager.getPage();
+            let current = before - 1;
+            if (current < 0) {
+                return;
+            }
+
+            list.passive(before);
+            form.passive(before);
+
+            list.active(current);
+            form.active(current);
+
+            pager.setPage(current);
+        });
+    });
+
+    let changeEvent = [
+        'mobile.app.record.create.change.ドロップダウン',
+        'mobile.app.record.create.change.ユーザー選択',
+    ];
+    kintone.events.on(changeEvent, (event) => {
+        let noInputsNum = form.groupList.length - 2;
+        console.log('noInputsNum', noInputsNum);
+        let value = event.changes.field.value;
+        let fieldCode = event.type.replace(/.*\./, '');
+        console.log(field, value);
+        console.log(form.grouPlist[noInputsNum]);
+        // ここ
+    });
+
+})(jQuery);
