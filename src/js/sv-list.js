5dda7e506ea2103cffc525bfc66e310922b44d7f
Yoshihiko Takeuchi <yoshihiko-takeuchi@cybozu.co.jp>
2019-06-10 13:18:27 +0900

    カスタマイズビュー対応

--- src/js/sv-list.js
+++ src/js/sv-list.js
@@ -7,6 +7,46 @@ jQuery.noConflict();
         return res;
     }
 
+    let createColumnList = (record) => {
+        let columnList = {};
+        let i = 0;
+        for (let fieldCode of Object.keys(record)) {
+            columnList[`column${i}`] = fieldCode;
+            i++;
+        }
+
+        return columnList;
+    }
+
+    let createValueNames = (columnList) => {
+        return Object.keys(columnList);
+    }
+
+    let createListHeader = (columnList) => {
+        let item = '<tr>';
+        for (let key of Object.keys(columnList)) {
+            item += `<td><div class="${key}"></div></td>`;
+        }
+        item += '</tr>';
+
+        return item;
+    }
+
+    let createItemList = (columnList, records) => {
+        let result = [];
+        for (let record of records) {
+            let item = {};
+            console.log(record);
+            for (let key of Object.keys(columnList)) {
+                let fieldCode = columnList[key];
+                item[key] = record[fieldCode].value;
+            }
+            result.push(item);
+        }
+
+        return result;
+    }
+
     kintone.events.on(['mobile.app.record.index.show'], (event) => {
         let records = event.records;
 
@@ -17,10 +57,34 @@ jQuery.noConflict();
 
         window.sv.saveLocalStorage(window.sv.lsListKey, recordIdList);
 
+
+
         if (event.viewId !== 5698734) {
             return event;
         }
-        console.log('kokokara customize view');
+
+
+        getRecords('').then((res) => {
+            let records = res.records;
+
+            let columnList = [];
+            for (let i = 0; i < records.length; i++) {
+                let record = records[i];
+                if (i === 0) {
+                    columnList = createColumnList(record);
+                }
+
+            }
+
+            let options = {
+                valueNames: createValueNames(columnList),
+                item: createListHeader(columnList)
+            };
+
+            let itemList = createItemList(columnList, records);
+
+            let list = new List('sv-list', options, itemList);
+        });
     });
 
 })(jQuery, kintone.$PLUGIN_ID);
